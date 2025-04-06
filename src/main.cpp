465a196af756be917aa764ffef23024829525e91
alex2835 <av.uschenko@gmail.com>
2025-04-06 21:15:46 +0400

    copy test

--- src/main.cpp
+++ src/main.cpp
@@ -138,5 +138,10 @@ int main(int, char**){
     Any object( Any::Array{ Any(), 1, 2, 3, 4.0f, "hello",
                             Any::Array{ "string", 5, 10.0 },
                             Any::Map{ { "str", 1 }, { 2, Any::Array{ 1, 2 } } } } );
+
+    auto copy = object;
+    copy.getArray().front() = 0;
     printAny( object );
+    std::println("");
+    printAny( copy );
 }
